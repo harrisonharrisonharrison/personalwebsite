d3fa5551d30a19b856899df1c362d95f9cb3e412
Harrison Tran <harrit4@uci.edu>
2025-06-14 23:10:12 -0700

    description box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,28 @@ import * as motion from "motion/react-client"
 
 export default function Home() {
   return (
-    <div className="ml-[30vw]">
-      <motion.div
-      initial={{ y: "0px", opacity: 0}}
-      animate={{ y: "100px", opacity: 1}}
-      >
-        Harrison Tran
-      </motion.div>
+    <div>
+      <div className="ml-[30vw]">
+          <motion.div
+            initial={{ y: "0px", opacity: 0}}
+            animate={{ y: "100px", opacity: 1}}
+          >
+            <div className="text-red-400">
+              harrison tran
+            </div>
+          </motion.div>
+      </div>
+      <div className="mr-[30vw]">
+          <motion.div
+            initial={{ y: "0px", opacity: 0}}
+            animate={{ y: "100px", opacity: 1}}
+            transition={{delay: 0.5}}
+          >
+            <div className="text-white">
+              harrison tran
+            </div>
+          </motion.div>
+      </div>
     </div>
   );
 }
