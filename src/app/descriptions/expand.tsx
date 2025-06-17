2e029c4476496cbcb4e3efdfbaf3d98fee5caf25
Harrison Tran <harrit4@uci.edu>
2025-06-17 03:44:55 -0700

    grammar

--- src/app/descriptions/expand.tsx
+++ src/app/descriptions/expand.tsx
@@ -36,7 +36,7 @@ export default function ReadMoreComponent() {
                 animate={{ y:10, opacity: 1}}>
             <p id="expand"  className="m-auto md:w-150 w-fit mt-60" >
             helloooo im harrison, a second year compsci major at uci, and this is my personal 
-            website/portfolio. here, i hope to add a project showcase and create gallery for my photos, among other things.
+            website/portfolio. here, i hope to add a project showcase and create a gallery for my photos, among other things.
             </p>
             <div className="text-center"><Desc/></div>
         </motion.div>
