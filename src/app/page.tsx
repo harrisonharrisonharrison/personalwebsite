fc0ea3ce77efc3eecae67e23b5a23cc9a0e1b0b2
Harrison Tran <harrit4@uci.edu>
2025-06-16 18:35:04 -0700

    hopefully fixed formatting :sob:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,78 +1,141 @@
 import * as motion from "motion/react-client"
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
+        
+      <div className="absolute  md:ml-[30vw] ml-[0] mt-20">
         <motion.div
           initial={{ scale:0, opacity: -1}}
           animate={{ scale:1, opacity: 1}}>
-          <div className="absolute ml-[30vw] mt-20">
+          <Image
+            src="/pfp2.jpg"
+            height={100}
+            width={100}
+            alt="my profile pic"
+            className="rounded-full aspect-2/3 object-cover"
+          />
+        </motion.div>
+        <motion.div
+          initial={{ y: "-500px", opacity: 0}}
+          animate={{ y: "-125px", opacity: 1}}
+          className="ml-30 text-red-400 "
+        >
+          harrison tran
+        </motion.div>
+          
+          <motion.div
+            initial={{ y: "0px", opacity: 0}}
+            animate={{ y: "-115px", opacity: 1}}
+            transition={{delay: 0.3}}
+          >
             <Image
-              src="/pfp2.jpg"
-              height={100}
-              width={100}
-              alt="my profile pic"
-              className="rounded-full aspect-2/3 object-cover"
+                src="/schoolico.png"
+                height={20}
+                width={20}
+                alt="school"
+                className="inline ml-30"
             />
-          </div>
+            <p className="text-white inline ml-2">
+              uc irvine
+            </p> 
+          </motion.div>
+          <motion.div
+            initial={{ y: "0px", opacity: 0}}
+            animate={{ y: "-115px", opacity: 1}}
+            transition={{delay: 0.5}}
+          >
+            <Image
+                src="/locoicon.png"
+                height={25}
+                width={25}
+                alt="school"
+                className="inline ml-30"
+            />
+            <p className="text-white inline ml-1">
+              garden grove, ca
+            </p>
+          </motion.div>
+          <motion.div
+            initial={{ y: "0px", opacity: 0}}
+            animate={{ y: "-115px", opacity: 1}}
+            transition={{delay: 0.7}}
+          >
+            <Image
+                src="/techstackico.png"
+                height={25}
+                width={25}
+                alt="school"
+                className="inline ml-30"
+            />
+            <p className="text-white inline ml-1">
+              c++, python, tailwind, java
+            </p> 
+          </motion.div>
+          
+      </div>
+
+      <div className="absolute ml-[89vw] md:ml-[70vw] mt-8 text-right">  
+        <motion.div
+          initial={{ y: "0px", opacity: 0}}
+          animate={{ y: "73px", opacity: 1}}
+          transition={{delay: .9}}
+        >
+          <Link href={'https://github.com/harrisonharrisonharrison'}>
+            <Image 
+              src="/github ico.png" 
+              alt="github" 
+              width={30} height={30}
+              className="ml-5"
+              />
+          </Link>
         </motion.div>
         <motion.div
           initial={{ y: "0px", opacity: 0}}
-          animate={{ y: "100px", opacity: 1}}
+          animate={{ y: "73px", opacity: 1}}
+          transition={{delay: 1.1}}
         >
-          <p className="relative text-red-400 ml-[37vw]">
-            harrison tran
-          </p>
-          <p className="absolute mt-[-25] text-blue-400 ml-[70vw]">
-            socials
-          </p>
+          <Link href={'https://steamcommunity.com/id/carrotoes/'}>
+            <Image 
+              src="/steamico.png" 
+              alt="steam" 
+              width={30} height={30}
+              className="ml-5"
+              />
+          </Link>
         </motion.div>
         <motion.div
           initial={{ y: "0px", opacity: 0}}
-          animate={{ y: "110px", opacity: 1}}
-          transition={{delay: 0.5}}
+          animate={{ y: "73px", opacity: 1}}
+          transition={{delay: 1.3}}
         >
-          <Image
-              src="/schoolico.png"
-              height={20}
-              width={20}
-              alt="school"
-              className="absolute ml-[550]"
-          />
-          <p className="text-white ml-[580]">
-            uc irvine
-          </p> 
-          <Image
-              src="/locoicon.png"
-              height={25}
-              width={25}
-              alt="school"
-              className="absolute ml-[36.9vw]"
-          />
-          <p className="text-white ml-[38.3vw]">
-            garden grove, ca
-          </p> 
-          <Image
-              src="/techstackico.png"
-              height={25}
-              width={25}
-              alt="school"
-              className="absolute ml-[36.9vw]"
-          />
-          <p className="text-white ml-[38.3vw]">
-            c++, python, tailwind, java
-          </p> 
+          <Link href={'https://www.instagram.com/lasnganga/'}>
+            <Image 
+              src="/instaico.png" 
+              alt="github" 
+              width={30} height={30}
+              className="ml-5"
+              />
+          </Link>
         </motion.div>
         <motion.div
           initial={{ y: "0px", opacity: 0}}
-          animate={{ y: "70px", opacity: 1}}
-          transition={{delay: 0.7}}
+          animate={{ y: "73px", opacity: 1}}
+          transition={{delay: 1.5}}
         >
-          <p className="absolute mt-1 ml-[70vw] text-blue-400 text-right">
-            desc
-          </p> 
+          <Link href={'https://www.linkedin.com/in/harrison-tran-547213294/'}>
+            <Image 
+              src="/linkedico.png" 
+              alt="github" 
+              width={30} height={30}
+              className="ml-5"
+              />
+          </Link>
         </motion.div>
+      </div>
+        
     </div>
   );
 }
