0fc41473cad0f0d1b20fcb880959f42d21d5d813
Harrison Tran <harrit4@uci.edu>
2025-06-16 17:06:11 -0700

    added profile picture, school, location, and techstack with animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,78 @@
 import * as motion from "motion/react-client"
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
-      <div className="ml-[30vw]">
-          <motion.div
-            initial={{ y: "0px", opacity: 0}}
-            animate={{ y: "100px", opacity: 1}}
-          >
-            <div className="text-red-400">
-              harrison tran
-            </div>
-          </motion.div>
-      </div>
-      <div className="mr-[30vw]">
-          <motion.div
-            initial={{ y: "0px", opacity: 0}}
-            animate={{ y: "100px", opacity: 1}}
-            transition={{delay: 0.5}}
-          >
-            <div className="text-white">
-              harrison tran
-            </div>
-          </motion.div>
-      </div>
+        <motion.div
+          initial={{ scale:0, opacity: -1}}
+          animate={{ scale:1, opacity: 1}}>
+          <div className="absolute ml-[30vw] mt-20">
+            <Image
+              src="/pfp2.jpg"
+              height={100}
+              width={100}
+              alt="my profile pic"
+              className="rounded-full aspect-2/3 object-cover"
+            />
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ y: "0px", opacity: 0}}
+          animate={{ y: "100px", opacity: 1}}
+        >
+          <p className="text-red-400 ml-[37vw]">
+            harrison tran
+          </p>
+          <p className="absolute mt-[-25] text-blue-400 ml-[70vw]">
+            socials
+          </p>
+        </motion.div>
+        <motion.div
+          initial={{ y: "0px", opacity: 0}}
+          animate={{ y: "110px", opacity: 1}}
+          transition={{delay: 0.5}}
+        >
+          <Image
+              src="/schoolico.png"
+              height={20}
+              width={20}
+              alt="school"
+              className="absolute ml-[37vw]"
+          />
+          <p className="text-white ml-[38.3vw]">
+            uc irvine
+          </p> 
+          <Image
+              src="/locoicon.png"
+              height={25}
+              width={25}
+              alt="school"
+              className="absolute ml-[36.9vw]"
+          />
+          <p className="text-white ml-[38.3vw]">
+            garden grove, ca
+          </p> 
+          <Image
+              src="/techstackico.png"
+              height={25}
+              width={25}
+              alt="school"
+              className="absolute ml-[36.9vw]"
+          />
+          <p className="text-white ml-[38.3vw]">
+            c++, python, tailwind, java
+          </p> 
+        </motion.div>
+        <motion.div
+          initial={{ y: "0px", opacity: 0}}
+          animate={{ y: "70px", opacity: 1}}
+          transition={{delay: 0.7}}
+        >
+          <p className="absolute mt-1 ml-[70vw] text-blue-400 text-right">
+            desc
+          </p> 
+        </motion.div>
     </div>
   );
 }
